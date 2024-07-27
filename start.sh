1c9b95a1b11805fa0650039ff0e334865d5dc37f
KuaterCraft <kuatercraft@gmail.com>
2024-07-27 18:59:24 +0530

    test2

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
+java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar /minecraft/server.jar
